f87c1b4eaa9ebf32a32ae4799cdb25456c1ddfca
AanSM <adansantos1100@gmail.com>
2026-04-12 23:41:45 -0400

    subiendo cambios

---
+++ js/home.js
@@ -0,0 +1,23 @@
+const containerTabs = document.querySelectorAll('.tab');
+const AllcontainerDishes = document.querySelectorAll('.container-dishes');
+
+containerTabs.forEach((tab)=>{
+    tab.addEventListener('click', e=>{
+        const tabName =  e.target.dataset.name;
+
+        
+        containerTabs.forEach(tab => tab.classList.remove('active'));
+        e.target.classList.add('active');
+
+        AllcontainerDishes.forEach(containerDishes => {
+            const dishName = containerDishes.dataset.name;
+
+            if (tabName=== dishName){
+                containerDishes.classList.add('active');
+            }
+            else{
+                containerDishes.classList.remove('active');
+            }
+        })
+    });
+});
